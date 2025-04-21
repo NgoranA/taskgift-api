cb65186e72a00a9acc5d6e305748321bcc171e72
NgoranA <ngoranaristide4@gmail.com>
2025-04-21 10:32:05 +0100

    added the get all tasks route

--- controllers/tasks-controller.js
+++ controllers/tasks-controller.js
@@ -1,12 +1,27 @@
 import { query } from "../config/db.js";
 import logger from "../utils/logger.js";
 
+export async function getAllTasks(req, res, nex) {
+  const userId = req.user.id
+  try {
+    const fetchTasksQuery = `SELECT description, title, created_at, updated_at, due_date FROM tasks 
+                              WHERE owner_id = $1 ORDER BY created_at DESC
+                            `
+    const result = await query(fetchTasksQuery, [userId])
+    logger.debug(`Fetched ${result.rows.length} tasks for user : ${userId}`)
+    return res.status(200).json(result.rows)
+  } catch (error) {
+    logger.error(`Error fetching tasks for user ${userId} : `, error)
+    return res.status(error.status || 500).json({ message: error.message || "Server error while fetching the tasks" })
+  }
+}
+
 export async function createTaskHandler(req, res, next) {
   const { title, description } = req.body
   const userId = req.user.id
   const due_date = new Date()
   try {
-    const insertionQuery = `
+    const insertTaskQuery = `
       INSERT INTO tasks (owner_id, title, description, due_date)
       VALUES ($1, $2, $3, $4)
       RETURNING *;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,12 @@
 import express from "express"
 import authMiddleware from "../middlewares/authmiddleware.js"
-import { createTaskHandler } from "../controllers/tasks-controller.js"
+import { createTaskHandler, getAllTasks, } from "../controllers/tasks-controller.js"
 import { createTaskValidator } from "../validators/create-task-validator.js"
 
 const router = express.Router()
 
 
-// router.get("/", authMiddleware, tasksHandler)
+router.get("/", authMiddleware, getAllTasks)
 
 router.post("/create-task", createTaskValidator, authMiddleware, createTaskHandler)
 
